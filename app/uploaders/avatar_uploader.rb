e58605e483ddb2998d9c4cdab87920d3b98498e8
Naureen  Rizvi <snr@Naureens-MacBook-Pro.local>
2017-01-02 14:46:53 -0500

    fixed aws problem

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -6,16 +6,16 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #include CarrierWave::RMagick
    include CarrierWave::MiniMagick
 
-if Rails.env.production
+if Rails.env.production?
   storage :fog
 else
   storage :file
 end
 
 
+
   # Choose what kind of storage to use for this uploader:
 
-  # storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
