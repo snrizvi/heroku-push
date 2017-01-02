ba066fd9ece3afd1f364069f04cb0e721d30b2e8
Naureen  Rizvi <snr@Naureens-MacBook-Pro.local>
2017-01-02 13:49:38 -0500

    added aws and fog

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -9,12 +9,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
 if Rails.env.production
   storage :fog
 else
-  sotarage :file
+  storage :file
 end
 
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
+
   # storage :fog
 
   # Override the directory where uploaded files will be stored.
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
-carrierwave.rb
+
 
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => "#{ENV['AKIAIKDIGPGZYOD55ZTA']}",
-    :aws_secret_access_key => "#{ENV['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']}",
-    :region                => "#{ENV['US Standard']}",
+    :aws_access_key_id     => "#{ENV['AWS_ACCESS_KEY_ID']}",
+    :aws_secret_access_key => "#{ENV['AWS_SECRET_ACCESS_KEY']}",
+    :region                => "#{ENV['S3_REGION']}",
     :path_style            => true
   }
 
-  config.fog_directory =  "#{ENV['US Standard']}"
-  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
+  config.fog_directory =  "#{ENV['S3_BUCKET_NAME']}"
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"   
 end
 
